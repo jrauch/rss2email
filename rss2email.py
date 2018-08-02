eb9daad0c31ad0481dba6fc4dd26a648479e0fe8
Jeremy Rauch <jrauch@latacora.com>
2018-08-02 18:55:45 -0400

    fix nvd pathological republish on very old posts that are updated on the site but still carry an old date - we probably don't care to resend an email in this case

--- rss2email.py
+++ rss2email.py
@@ -75,7 +75,7 @@ def rss_to_email_handler(event, context):
 					email["subject"] = "{}{}".format(preamble,entry.title)
 					email["body"] = "{} (details at: {})".format(entry.summary if "summary" in entry else "", entry.link)
 					print("{} {} {}".format(email["subject"], pubdate, time))
-					#send_email(email)
+					send_email(email)
 
 			s3.put_object(Bucket=bucketname, Key=old_feed_hash, Body=f.text)
 
